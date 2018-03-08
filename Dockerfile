c617cce809f3eaedd2e733adcf7b5e848c219b98
Kevin Li <likevi@microsoft.com>
2018-03-08 13:04:12 -0800

    fix cant create directory in image

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,4 @@ RUN cd /go/src/sblogdriver && go get && go build -o /usr/bin/sblogdriver
 
 FROM scratch 
 COPY --from=build /usr/bin/sblogdriver /usr/bin/sblogdriver
-RUN mkdir -p /run/docker/plugins # needed to communicate with Docker, this is where the jsonfile.sock will be
 ENTRYPOINT ["/usr/bin/sblogdriver"]
--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ id=$(docker create rootfsimage true)
 mkdir -p myplugin/rootfs
 sudo docker export "$id" | tar -x -C myplugin/rootfs
 cp config.json myplugin/config.json
+mkdir -p myplugin/rootfs/run/docker/plugins # needed to communicate with Docker, this is where the jsonfile.sock will be
 docker rm -vf "$id"
 docker rmi rootfsimage
