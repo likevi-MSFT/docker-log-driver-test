e13ac66b5d56700071e3efa8e5968a9d9bbf3d84
Kevin Li <likevi@microsoft.com>
2018-04-02 20:27:28 -0700

    update installplugin script to v1.2

--- installplugin.sh
+++ installplugin.sh
@@ -1,2 +1,2 @@
-docker plugin create likevi/sblogdriver:1.3 myplugin
-docker plugin enable likevi/sblogdriver:1.3
+docker plugin create likevi/sblogdriver:1.2 myplugin
+docker plugin enable likevi/sblogdriver:1.2
